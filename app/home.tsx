431950ce6edcb9a41aa2d3020338124c1a2d7ed6
BdNWoG <billy.gao@hotmail.com>
2024-09-21 20:47:07 -0700

    step

---
+++ app/home.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import className from 'twrnc'
+
+const home = () => {
+    return (
+        <View>
+            {/* header */}
+            <View>
+                
+            </View>
+        </View>
+    )
+}
+
+export default home
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Pressable } from 'react-native'
 import React from 'react'
 import className from 'twrnc'
+import { useRouter } from 'expo-router'
 
 const index = () => {
+
+    const router = useRouter();
+
     return (
-        <View style={className`bg-orange-500 flex-1 justify-center items-center`}>
-            <Image source={require('../assets/logo.png')}/>
+        <Pressable onPress={() => router.push('/home')} style={className`bg-orange-500 flex-1 justify-center items-center`}>
+            <Image source={require('../assets/logo.png')} style={className`w-50 h-50`}/>
             <Text style={className`text-white text-6xl font-bold`}>Foodie</Text>
             <Text style={className`text-white text-lg font-semibold mt-2`}>Welcome to Foodie</Text>
-        </View>
+        </Pressable>
     )
 }
 
